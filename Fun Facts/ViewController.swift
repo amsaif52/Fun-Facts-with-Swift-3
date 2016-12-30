73d20852aa44efcc52e65962494138f298a43119
amsaif52 <am52saif@gmail.com>
2016-12-30 00:04:19 -0500

    Added Button, Label and Facts to the App

--- Fun Facts/ViewController.swift
+++ Fun Facts/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    let factProvider = FactProvider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factProvider.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFact() {
+        funFactLabel.text = factProvider.randomFact()
+    }
 
 }
 
