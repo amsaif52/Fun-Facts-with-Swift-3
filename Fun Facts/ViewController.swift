23f6e2b4a290ee746055bb0679f77272ebfbf2b5
amsaif52 <am52saif@gmail.com>
2016-12-29 22:21:39 -0500

    Initial Commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Ali Asgar Merchant on 12/29/16.
+//  Copyright © 2016 Ali Asgar Merchant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
