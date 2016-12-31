b4d411ad7ee88fe0fb9a7e803af2d5c7f03388f3
amsaif52 <am52saif@gmail.com>
2016-12-30 23:59:21 -0500

    Added Colors and Position and Size for items inside the view

--- Fun Facts/ViewController.swift
+++ Fun Facts/ViewController.swift
@@ -11,11 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
     let factProvider = FactProvider()
+    let colorProvider = BackgroundColorProvider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        funFactLabel.text = factProvider.randomFact()
+        funFactLabel.text = factProvider.facts[0]
+        view.backgroundColor = colorProvider.colors[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +29,9 @@ class ViewController: UIViewController {
 
     @IBAction func showFact() {
         funFactLabel.text = factProvider.randomFact()
+        let randomColor = colorProvider.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
     }
 
 }
